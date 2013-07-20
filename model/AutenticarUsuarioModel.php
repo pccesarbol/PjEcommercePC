ee5b91dd150afde0bc2106a2e7d68281d6f03ff8
Paulo Augusto <pccesaraugusto@email.com>
2013-07-20 20:55:33 -0300

    Criação dos arquivos de aurtenticação dos usuários

---
+++ model/AutenticarUsuarioModel.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AutenticarUsuarioModel
+ *
+ * @author Paulo
+ */
+class AutenticarUsuarioModel {
+    //put your code here
+}
+
+?>
