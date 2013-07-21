e6e1f61ede762554559a7bcea62309a388850ea4
Paulo Augusto <pccesaraugusto@email.com>
2013-07-21 15:54:55 -0300

    Atualização da tela index.php para venda de bolsas do ecommerce do pc.

--- index.php
+++ index.php
@@ -1,16 +1,126 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
+    
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <title>Rose Bolsas - Adiquira a sua.</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="views/css/bootstrap.min.css" rel="stylesheet">
+        
+        <style type="text/css">
+            body{padding-top: 60px;padding-bottom: 40px;}.sidebar-nav{padding: 9px 0;}@media (max-width: 980px){/* Enable use of floated navbar text */ .navbar-text.pull-right{float: none;padding-left: 5px;padding-right: 5px;}}
+        </style>
+        <link href="views/css/bootstrap-responsive.css" rel="stylesheet">
+        
+        <script src="views/js/bootstrap.min.js"></script>
     </head>
-    <body>
-        <?php
-        echo 'Teste do pc';
-        ?>
+    
+    <body>     
+      <form name="Frm1" action="" method="post"> 
+          
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <!-- Definindo o cabeçalhop da página -->
+                <img src="views/img/cabecalho_ecommerce.png"/>
+                <div class="container-fluid">
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+                    </button><a class="brand" href="#">Acompanhe seu(s) pedido(s)</a>
+                    <div class="nav-collapse collapse">
+                        <p class="navbar-text pull-right"><a href="#" class="navbar-link"><b>Registra-se</b></a>
+                        </p>
+                        <ul class="nav">
+                            <li class="active"><a href="#">Nossos produtos</a>
+                            </li>
+                            <li><a href="#about">Chat on-line</a>
+                            </li>
+                            <li><a href="#contact">Fale com vendedor</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span3">
+                    <div class="well sidebar-nav">
+                        <ul class="nav nav-list"><br/><br/><br/><br/>
+                            <li class="nav-header">Área do Cliente</li>
+                            <li class="active"><a href="#">Entrar</a>
+                            </li>                            
+                            <li class="nav-header">Área Administrativa</li>
+                            <li><a href="#">Entrar</a>
+                            </li>
+                            <li><a href="#">Chat on-line</a>
+                            </li>
+                            <li><a href="#">Fale com vendedor</a>
+                            </li>
+                            <li><a href="#">Telefones</a>
+                            </li>                             
+                        </ul>
+                    </div>
+                </div>
+                <div class="span9">
+                    <div class="hero-unit">
+                        
+                        <div class="row-fluid">
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 1.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 2.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 3.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row-fluid">
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 4.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 5.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                        <div class="span4">
+                            <h2>Heading</h2>
+                            <p>Bolsa 6.</p>
+                            <p><a class="btn" href="#">View details &raquo;</a>
+                            </p>
+                        </div>
+                    </div>
+                       
+                    </div>
+                    
+                </div>
+            </div>
+            <hr>
+            <footer>
+                <!-- Definindo o RODAPÉ da página -->
+                <img src="views/img/curves_01_1.png"/>
+            </footer>
+        </div>
+        <!-- Le javascript==================================================-->
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.js"></script>
+        
+     </form>   
     </body>
+
 </html>
