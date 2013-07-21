20cd2d763980d99fe8d2eb5ff2e4b1a10f913d89
Paulo Augusto <pccesaraugusto@email.com>
2013-07-20 21:20:41 -0300

    Criação das entidades e adição da classe usuário.

---
+++ entity/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Usuario
+ *
+ * @author Paulo
+ */
+class Usuario {
+    //put your code here
+}
+
+?>
