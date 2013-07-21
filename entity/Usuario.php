402cc8d1f2bef661a3936254918ff98b5861e98d
Paulo Augusto <pccesaraugusto@email.com>
2013-07-20 23:56:11 -0300

    Criação das entidades e adição da classe usuário.

--- entity/Usuario.php
+++ entity/Usuario.php
@@ -7,11 +7,36 @@
 
 /**
  * Description of Usuario
- *
+ * Esta classe tem como objetivo gerenciar os atributos dos usuários
  * @author Paulo
  */
 class Usuario {
-    //put your code here
+    /**
+     * Definindo os atributos da classe
+     * **/
+    public $idUsuario;
+    public $nomeUsuario;
+    public $senha;
+    public $dataCadastro;
+    public $email;
+    public $perfil;
+    public $telefone;
+    public $celular;
+    
+    /**
+     * Definindo o construtor da classe
+     * **/
+    public function __construct() {
+        
+    }
+    
+    /**
+     * Definindo o destrutor da classe
+     * **/
+    public function __destruct() {
+        
+    }
+    
 }
 
 ?>
