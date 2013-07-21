a25eec32b05dc8f282897794294c452b9aca6bde
Paulo Augusto <pccesaraugusto@email.com>
2013-07-21 17:33:42 -0300

    Atualização da tela index.php para venda de bolsas do ecommerce do pc.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     
     <head>
         <meta charset="utf-8">
-        <title>Rose Bolsas - Adiquira a sua.</title>
+        <title>FREELANCER/DESENVOLVEDOR.</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
@@ -26,16 +26,16 @@
                 <img src="views/img/cabecalho_ecommerce.png"/>
                 <div class="container-fluid">
                     <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                    </button><a class="brand" href="#">Acompanhe seu(s) pedido(s)</a>
+                    </button><a class="brand" href="#">Solicitar orçamento</a>
                     <div class="nav-collapse collapse">
                         <p class="navbar-text pull-right"><a href="#" class="navbar-link"><b>Registra-se</b></a>
                         </p>
                         <ul class="nav">
-                            <li class="active"><a href="#">Nossos produtos</a>
+                            <li class="active"><a href="#">Acompanhar seu pedido</a>
                             </li>
-                            <li><a href="#about">Chat on-line</a>
+                            <li><a href="#about">Ajuda on-line</a>
                             </li>
-                            <li><a href="#contact">Fale com vendedor</a>
+                            <li><a href="#contact">Fale com o desenvolvedor</a>
                             </li>
                         </ul>
                     </div>
@@ -50,15 +50,13 @@
                             <li class="nav-header">Área do Cliente</li>
                             <li class="active"><a href="#">Entrar</a>
                             </li>                            
-                            <li class="nav-header">Área Administrativa</li>
-                            <li><a href="#">Entrar</a>
-                            </li>
-                            <li><a href="#">Chat on-line</a>
-                            </li>
-                            <li><a href="#">Fale com vendedor</a>
+                            <li class="nav-header">Web Developer</li>
+                            <li><a href="#">Contato</a>
                             </li>
                             <li><a href="#">Telefones</a>
-                            </li>                             
+                            </li>                      
+                            <li><a href="#">Login</a>
+                            </li>  
                         </ul>
                     </div>
                 </div>
@@ -68,55 +66,46 @@
                         <div class="row-fluid">
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 1.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Sistema de Cadastro.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 2.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Controle de Estoque.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 3.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Manutenção de Páginas WEB.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
                     </div>
                     <div class="row-fluid">
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 4.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Manutenção de Sistemas Desktop.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 5.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Migração de Dados.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
                         <div class="span4">
                             <h2>Heading</h2>
-                            <p>Bolsa 6.</p>
-                            <p>Valor.</p>
-                            <p><a class="btn-primary" href="#">Comprar &raquo;</a>
+                            <p>Mala Direta.</p>                           
+                            <p><a class="btn-primary" href="#">Detalhar &raquo;</a>
                             </p>
                         </div>
-                    </div>
-                       
-                    </div>
-                    
+                    </div>                       
+                    </div>                    
                 </div>
             </div>
-            <hr>
             <footer>
                 <!-- Definindo o RODAPÉ da página -->
                 <img src="views/img/curves_01_1.png"/>
@@ -124,9 +113,7 @@
         </div>
         <!-- Le javascript==================================================-->
         <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.js"></script>
-        
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.0/js/bootstrap.js"></script>        
      </form>   
     </body>
-
 </html>
